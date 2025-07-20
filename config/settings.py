68ae9f6eee0557cd78a06610dcc1198c45ac8b94
Olga Kazorina <nefertituolga@gmail.com>
2025-07-20 12:37:10 +0300

    add settings celery, django-celery-beat, redis brokers, eventlet

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework_simplejwt",
     "drf_yasg",
+    "django-celery-beat",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,26 @@ SIMPLE_JWT = {
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+CACHE_ENABLED = False
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": os.getenv("LOCATION"),
+            "TIMEOUT": 300,
+            "KEY_PREFIX": "mailing_service",
+        }
+    }
+
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'myapp.tasks.my_task',
+        'schedule': timedelta(minutes=10),
+    },
+}
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
